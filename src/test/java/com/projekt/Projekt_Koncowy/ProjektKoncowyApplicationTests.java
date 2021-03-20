3dc6fd51ca44239611f53a8da1ec022ca1a9ee30
monik <Maalkolm>
2021-03-20 21:20:24 +0100

    Początek projektu

---
+++ src/test/java/com/projekt/Projekt_Koncowy/ProjektKoncowyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projekt.Projekt_Koncowy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjektKoncowyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
